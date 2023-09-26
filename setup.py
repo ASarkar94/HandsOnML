a89afac01b758c9150a51323d8b92905ef26c532
Arkadeep Sarkar <arkadeep1994@gmail.com>
2023-09-26 14:44:48 +0530

    Create base directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for storing code practiced while following Hands On ML book',
+    author='Arkadeep_Sarkar',
+    license='MIT',
+)
